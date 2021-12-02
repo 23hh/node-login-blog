54c4d651b07202a15a7f86d43c79ef8aded6ba0f
23h <lsh6166@gmail.com>
2021-12-02 21:01:36 +0900

    수정기능완료

--- renders/index.js
+++ renders/index.js
@@ -1,70 +1,61 @@
-const express = require('express');
+const express = require("express");
 const Posts = require("../models/posts");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    try{
-        res.render("board");
-    } catch (error) {
-        res.render('error');
-    }
+router.get("/", (req, res, next) => {
+  try {
+    res.render("board");
+  } catch (error) {
+    res.render("error");
+  }
 });
 
-router.get('/login', (req, res, next) => {
-    try{
-        res.render("login");
-    } catch (error) {
-        res.render('error');
-    }
+router.get("/login", (req, res, next) => {
+  try {
+    res.render("login");
+  } catch (error) {
+    res.render("error");
+  }
 });
 
-router.get('/sign', (req, res, next) => {
-    try{
-        res.render("sign");
-    } catch (error) {
-        res.render('error');
-    }
+router.get("/sign", (req, res, next) => {
+  try {
+    res.render("sign");
+  } catch (error) {
+    res.render("error");
+  }
 });
 
-
-
-router.get('/write', (req, res, next) => {
-    try{
-        res.render("write");
-    } catch (error) {
-        res.render('error');
-    }
+router.get("/write", (req, res, next) => {
+  try {
+    res.render("write");
+  } catch (error) {
+    res.render("error");
+  }
 });
 
-router.get('/posts/:postId', async (req, res, next) => {
-    try{
-        const { postId } = req.params;
-        const post = await Posts.findOne({ postId: postId }).exec();
-        console.log(post)
-        res.render("post", post);
-    } catch (error) {
-        res.render('error');
-    }
+router.get("/posts/:postId", (req, res, next) => {
+  try {
+    res.render("post");
+  } catch (error) {
+    res.render("error");
+  }
 });
 
-
-router.get('/modify/:postId', async (req, res, next) => {
-    try{
-        const postId = req.params;
-        res.render("post", {Posts});
-    } catch (error) {
-        res.render('error');
-    }
+router.get("/modify/:postId", async (req, res, next) => {
+  try {
+    res.render("modify");
+  } catch (error) {
+    res.render("error");
+  }
 });
 
-router.get('/error', (req, res, next) => {
-    try{
-        res.render("error");
-    } catch (error) {
-        res.sendStatus(404).send(
-            {errorMessage: "페이지를 찾을 수 없습니다."}
-        )
-    }
+router.get("/error", (req, res, next) => {
+  try {
+    res.render("error");
+  } catch (error) {
+    res.sendStatus(404).send({ errorMessage: "페이지를 찾을 수 없습니다." });
+  }
 });
 
 module.exports = router;
--- routers/router_posts.js
+++ routers/router_posts.js
@@ -11,6 +11,7 @@ const postSchema = Joi.object({
   content: Joi.string().required(),
 });
 
+//게시물 정보 조회
 router.route("/").get(async (req, res, next) => {
   const posts = await Posts.find({}).sort("-date");
   res.json({ posts: posts });
@@ -20,16 +21,62 @@ var moment = require('moment');
 require('moment-timezone');
 moment.tz.setDefault('Asia/Seoul');
 
+//게시글저장
 router.post("/", authMiddleware, async (req, res) => {
   const { title, content } = await req.body;
   const nickname = res.locals.user.nickname;
   const userId = res.locals.user.userId;
-  console.log(nickname)
 
   const date = moment().format('YYYY-MM-DD HH:mm:ss');
 
   await Posts.create({ nickname, userId, title, content, date});
   res.send({ result: 'success' });
+});
+
+//상세페이지조회
+router.get('/:postId', authMiddleware, async (req, res, next) => {
+  try{
+      const { postId } = req.params;
+      const post = await Posts.findOne({ postId: postId }).exec();
+      res.json({ detail : post });
+  } catch (error) {
+      res.render('error');
+  }
+});
+
+//수정게시물 보여주기
+// router.get("/:postId", authMiddleware, async (req, res, next) => {
+//   try {
+//     const { postId } = req.params;
+//     console.log(postId)
+//     const post = await Posts.findOne({ postId: postId }).exec();
+//     console.log(post)
+//     res.json({ modify: post });
+//   } catch (error) {
+//     res.render("error");
+//   }
+// });
+
+//게시글 수정
+router.patch("/:postId", async (req, res) => {
+  const { postId } = req.params;
+  const { title, content } = req.body;
+
+  const isIdInBoard = await Posts.find({ postId });
+  if (isIdInBoard.length > 0) {
+    await Posts.updateOne({ postId: postId }, { $set: { title, content } });
+  } 
+  res.send({ result: "success" });
+});
+
+router.delete("/:postId", async (req, res) => {
+  const { postId } = req.params;
+
+  const isIdInBoard = await Posts.find({ postId });
+  if (isIdInBoard.length > 0) {
+    await Posts.deleteOne({ postId });
+  }
+  res.send({ result: "success" });
 });
 
 module.exports = router;
